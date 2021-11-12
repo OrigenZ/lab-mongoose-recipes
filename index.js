fa02640de35d41cabe37f3dfb7683015a2cd4d4b
Sherab Pereira <sherabpereira@gmail.com>
2021-11-12 14:45:36 +0100

    Lab refactor to async await

--- index.js
+++ index.js
@@ -1,72 +1,66 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require("./models/Recipe.model");
+const Recipe = require('./models/Recipe.model')
 // Import of the data from './data.json'
-const recipes = require("./data");
-const recipe = require("./recipe");
+const recipes = require('./data')
+const recipe = require('./recipe')
 
-const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
+const MONGODB_URI = 'mongodb://localhost:27017/recipe-app'
 
-const successInsertCallback = (data) => {
-  console.log("\nSuccesful recipe insertion: \n");
-  for (const recipe of data) console.log(recipe.title);
-};
-const successUpdateCallback = (data) =>
-  console.log("\nRecipe: " + data.title + " updated\n");
+const successInsertHandler = (data) => {
+  console.log('\nSuccesful recipe insertion: \n')
+  for (const recipe of data) console.log(recipe.title)
+}
+const successUppdateHandler = (data) =>
+  console.log('\nRecipe: ' + data.title + ' updated\n')
 
-const successDeleteCallback = (data) => {
-  if (data.deletedCount > 0) console.log("\nRecipe successfuly deleted\n");
-  else console.log("\nRecipe to be deleted could not be found\n");
-};
-const errorCallback = (error) => console.log("An error occurred", error);
+const successDeleteHandler = (data) => {
+  if (data.deletedCount > 0) console.log('\nRecipe successfuly deleted\n')
+  else console.log('\nRecipe to be deleted could not be found\n')
+}
+
 
 // Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(async () => {
+const createRecipe = async () => {
+
+  try {
+    // CONNECT
+    await mongoose.connect(MONGODB_URI, {})
+
     // SINGLE RECIPE
-    await Recipe.create(recipe).then((recipe) => successInsertCallback(recipe));
-  })
-  .then(async () => {
+    const recipeFromDb = await Recipe.create(recipe)
+    successInsertHandler(recipeFromDb)
+
     // MULTIPLE RECIPES
-    await Recipe.insertMany(recipes).then((recipes) =>
-      successInsertCallback(recipes)
-    );
-  })
-  .then(async () => {
+    const recipesFromDb = await Recipe.insertMany(recipes)
+    successInsertHandler(recipesFromDb)
+
     // UPDATING A SINGLE DOCUMENT
-    await Recipe.findOneAndUpdate(
-      { title: "Rigatoni alla Genovese" },
-      { duration: 100 },
-      { new: true }
-    ).then((updatedRecipe) => successUpdateCallback(updatedRecipe));
-  })
-  .then(async () => {
+    const updatedRecipe = await Recipe.findOneAndUpdate(
+      { title: 'Rigatoni alla Genovese' },
+      { duration: 6 },
+      { new: true },
+    )
+    successUppdateHandler(updatedRecipe)
+
     //DELETING A SINGLE DOCUMENT
-    await Recipe.deleteOne({ title: "Carrot Cake" }).then((deletedRecipe) =>
-      successDeleteCallback(deletedRecipe)
-    );
-  })
-  .catch((error) => {
-    errorCallback(error.message);
-  })
-  .finally(() => {
-    console.log(mongoose.connection.readyState )
-    if (mongoose.connection.readyState === 1) mongoose.connection.close();
-    console.log(mongoose.connection.readyState )
-  });
+    const deletedRecipe = await Recipe.findOneAndDelete({ title: 'Carrot Cake' })
+    successDeleteHandler(deletedRecipe)
 
+  } catch (err) {
+    console.log('An error occurred', err.message)
+  } finally {
+    //Ready states
+    // 0: disconnected
+    // 1: connected
+    // 2: connecting
+    // 3: disconnecting
 
-  //Ready states
+    //console.log(mongoose.connection.readyState)
+    if (mongoose.connection.readyState === 1) mongoose.connection.close()
+    //console.log(mongoose.connection.readyState)
+  }
 
-  // 0: disconnected
-  // 1: connected
-  // 2: connecting
-  // 3: disconnecting
+}
 
- 
+createRecipe()
